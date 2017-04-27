b551ac7c0c37eb4e073ffc6a891eabe52bceb75d
Aarni Koskela <akx@iki.fi>
2017-04-27 10:52:46 +0300

    Slide 17

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,27 @@ module.exports = {
 	output: {
 		path: __dirname + '/dist',
 		filename: 'bundle.js',
-	},};
+	},
+	module: {
+		rules: [
+			{
+				test: /.styl$/,
+				use: ['style-loader', 'css-loader', 'stylus-loader'],
+			},
+			{
+				test: /.(jpg|jpeg|png|mp3)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /.jsx?$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env', 'react'],
+					},
+				},
+				exclude: /node_modules/,
+			},
+		],
+	},
+};
