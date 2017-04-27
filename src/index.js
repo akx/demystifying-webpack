c4633470acd698f2f5fa8bb40db556b5c3d678f0
Aarni Koskela <akx@iki.fi>
2017-04-27 10:53:37 +0300

    Slide 18 (with extra MP3)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CatImageURL from './cat.png';
+import MiauSoundURL from './miau.mp3';
 
-document.write('herlo worl');
+const playMiau = () => {
+	const el = Object.assign(
+		document.createElement('audio'),
+		{
+			src: MiauSoundURL,
+			autoPlay: false,
+			playbackRate: 0.5 + Math.random()
+		}
+	);
+	document.body.appendChild(el);
+	el.play();
+	// TODO: clean up the stray cats
+};
 
+class CatApp extends React.Component {
+	constructor() {
+		super();
+		this.state = {number: 1};
+	}
+	render() {
+		const cats = [];
+		for(let i = 0; i < this.state.number; i++) {
+			cats.push(
+				<img
+				onMouseOver={playMiau}	
+				src={CatImageURL}
+				key={i}
+				/>);
+		}
+		return (
+			<div>
+				<audio src={MiauSoundURL} autoPlay={false} ref={(el) => this.audio = el} />
+				<h1>Cat app</h1>
+				<button
+					onClick={(e) => this.setState({number: this.state.number + 1})}>More mew!</button>
+				<br />
+				{cats}
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<CatApp />, document.body);
